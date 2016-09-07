4fd738d104fc5c2e7bb0a9fd591ec742b31c42e6
mashers <mashley@me.com>
2016-09-07 09:06:49 +0100

    PIN is now loaded from file on SD card

--- payload_stage2/source/main.c
+++ payload_stage2/source/main.c
@@ -60,72 +60,135 @@ void drawPin(char * entered) {
 
 void main()
 {
+	/*
+	Mount SD card
+	*/
     FATFS fs;
     f_mount(&fs, "0:", 0); //This never fails due to deferred mounting
     
+    /*
+    Screen init
+    */
     prepareForBoot();
     
-    int pinPos = 0;
-    char pin[] = "BARRAL";
-    int pinlen = strlen(pin);
-    char entered[pinlen+1];// = "----";
-    
-    for (int i=0; i<pinlen; i++) {
-    	entered[i] = '-';
+	FIL pinFile;
+
+	/*
+	Try to open the PIN file
+	*/
+    if(f_open(&pinFile, "pin.txt", FA_READ) == FR_OK) {
+    	/*
+    	Read the PIN file into the buffer 'pin'
+    	*/
+		unsigned int br;
+		char pin[f_size(&pinFile)];
+		int pinlen = f_size(&pinFile);
+		
+		for (int i=0; i<pinlen; i++) {
+			pin[i] = '-';
+		}
+
+        f_read(&pinFile, pin, f_size(&pinFile), &br);
+        f_close(&pinFile);
+        
+        pin[pinlen] = '\0';
+        
+        /*
+        DEBUG: print the read PIN on the screen
+        */
+        /*
+	    drawString(pin, 10, 10, COLOR_RED);
+		waitInput();
+		clearScreens();
+		*/
+		
+		/*
+		Start at the beginning of the PIN and with an empty entry
+		*/
+		int pinPos = 0;
+		char entered[pinlen+1];
+	
+		for (int i=0; i<pinlen; i++) {
+			entered[i] = '-';
+		}
+	
+		entered[pinlen] = '\0';
+	
+		/*
+		While the user has not finished entering the PIN...
+		*/	
+		while (pinPos < pinlen) {
+			//Draw the prompt to enter the PIN and whatever has already been entered
+			drawPin(entered);
+	
+			//Buffer for the entered character
+			char append;
+			
+			//Wait for the user to press a button
+			u32 key = waitInput();
+			
+			//Translate the pressed button to a character in the PIN
+			if (key == BUTTON_A) {
+				append = 'A';
+			}
+			else if (key == BUTTON_B) {
+				append = 'B';
+			}
+			else if (key == BUTTON_X) {
+				append = 'X';
+			}
+			else if (key == BUTTON_Y) {
+				append = 'Y';
+			}
+			else if (key == BUTTON_UP) {
+				append = 'U';
+			}
+			else if (key == BUTTON_DOWN) {
+				append = 'D';
+			}
+			else if (key == BUTTON_LEFT) {
+				append = 'L';
+			}
+			else if (key == BUTTON_RIGHT) {
+				append = 'R';
+			}
+			else {
+				append = '-';
+			}
+		
+			/*
+			The user pressed a button which can exist in the PIN
+			*/
+			if (append != '-') {
+				//Change the character in the entered PIN at the current position to the character entered
+				entered[pinPos] = append;		
+				//Go to the next character position in the PIN
+				pinPos++;
+			}
+		}
+	
+		/*
+		If the entered PIN matches what is expected, do one final screen refresh and then boot the A9LH payload from SD card
+		*/
+		if (strcmp(pin, entered) == 0) {
+			drawPin(entered);
+			bootPayload();
+		}
+		
+		/*
+		If the entered PIN does not match what is expected, show an error
+		*/
+		else {
+			clearScreens();
+			error("Incorrect PIN");
+		}
     }
     
-    entered[pinlen] = '\0';
-    
-//     drawString(entered, 10, 30, COLOR_WHITE);
-    
-    while (pinPos < pinlen) {
-    	drawPin(entered);
-    
-    	char append;
-    
-    	u32 key = waitInput();
-    	
-    	if (key == BUTTON_A) {
-    		append = 'A';
-    	}
-    	else if (key == BUTTON_B) {
-    		append = 'B';
-    	}
-    	else if (key == BUTTON_X) {
-    		append = 'X';
-    	}
-    	else if (key == BUTTON_Y) {
-    		append = 'Y';
-    	}
-    	else if (key == BUTTON_UP) {
-    		append = 'U';
-    	}
-    	else if (key == BUTTON_DOWN) {
-    		append = 'D';
-    	}
-    	else if (key == BUTTON_LEFT) {
-    		append = 'L';
-    	}
-    	else if (key == BUTTON_RIGHT) {
-    		append = 'R';
-    	}
-    	else {
-    		append = '-';
-    	}
-    	
-    	if (append != '-') {
-    		entered[pinPos] = append;		
-			pinPos++;
-    	}
-    }
-    
-    if (strcmp(pin, entered) == 0) {
-    	drawPin(entered);
-    	bootPayload();
-    }
+    /*
+    No PIN file exists. Just boot the payload
+    */
     else {
-    	clearScreens();
-    	error("Incorrect PIN");
-//     	bootPayload();
-    }    
+	    bootPayload();
+    }
+    
 }
